a94f1cbc0d1e6edb1c39268a9f21dc5672ef640f
Bruno Antunes <bmvantunes@users.noreply.github.com>
2021-12-17 20:03:27 +0000

    asdasdasdadsasd

--- apps/my-next-js/pages/ssr.tsx
+++ apps/my-next-js/pages/ssr.tsx
@@ -1,13 +1,36 @@
 import { GetServerSideProps } from "next"
 
-export default function SSR(props: unknown) {
-  return <div>{JSON.stringify(props)}</div>
-}
-
 export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       data: new Date().toISOString()
     }
   }
+}
+
+
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function TranslationsComponent(props: unknown) {
+  const { locale } = useRouter();
+
+  return (
+    <div>
+      <h5>{locale}</h5>
+
+      <Link href="/ssr" locale="es">
+        <a style={{ marginLeft: '20px' }}>/ES</a>
+      </Link>
+
+      <Link href="/ssr" locale="en">
+        <a style={{ marginLeft: '20px' }}>/EN</a>
+      </Link>
+
+      <pre>{JSON.stringify(props, null, 4)}</pre>
+
+      {/* <pre>{t('HELLO')}</pre> */}
+    </div>
+  );
 }
